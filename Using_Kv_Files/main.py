7254607d3562f7df0b16b25feffc2b87995dac37
Ryan <74478729+hootloot@users.noreply.github.com>
2021-11-08 12:54:26 -0500

    Update main.py

--- Using_Kv_Files/main.py
+++ Using_Kv_Files/main.py
@@ -1,7 +1,10 @@
 #This way of using the Kivy module is different
-#It uses type .kv file for the user interface
 
+#It uses type .kv file for the front end
 
+#But uses the .py files for the back end, for example this file
+
+#import the following modules
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
@@ -11,19 +14,20 @@ from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 
-
+#This is the front end part of the application, however it uses the .kv file in this folder
 class MyGrid(Widget):
     name = ObjectProperty(None)
     email = ObjectProperty(None)
-
+    
+    
+    
+#This is the back end of the application, the main focus of this file.
     def btn(self):
         print("Name:", self.name.text, "email:", self.email.text)
         self.name.text = ""
         self.email.text = ""
 
-
-
-
+#Again, these part are to build the application, these are needed
 class MyApp(App):
     def build(self):
         return MyGrid()
@@ -31,3 +35,10 @@ class MyApp(App):
 
 if __name__ == "__main__":
     MyApp().run()
+    
+    
+#Try running this file, try using it. 
+#Then try changing parts in the btn() function, see what else you can output. Using things you have already learnt in python, print statements, loops, if statements
+
+
+
