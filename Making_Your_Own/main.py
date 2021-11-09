ab6c8cbe92b7af224be56991d463a357a3dd3c8c
Ryan <74478729+hootloot@users.noreply.github.com>
2021-11-09 12:38:39 -0500

    Update main.py

--- Making_Your_Own/main.py
+++ Making_Your_Own/main.py
@@ -1,1 +1,30 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
+#This is the front end part of the application, however it uses the .kv file in this folder. 
+class MyGrid(Widget):
+    name = ObjectProperty(None)
+    email = ObjectProperty(None)
+    
+    
+    
+#This is the back end of the application, the main focus of this file.
+    def btn(self):
+        print("Name:", self.name.text, "email:", self.email.text)
+        self.name.text = ""
+        self.email.text = ""
+
+#Again, these part are to build the application, these are needed
+class MyApp(App):
+    def build(self):
+        return MyGrid()
+
+
+if __name__ == "__main__":
+    MyApp().run()
